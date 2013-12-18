a7936b2d75ea35eea52c325b0e3423ca5bc45209
Josh Rucker <jrrucker@ncsu.edu>
2013-12-18 15:49:17 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Utility Prototype</title>
+	<link href="css/main.css" type="text/css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+	
+	<div id="ncstate-util">
+	
+		<div class="ncstate-util-wrapper">
+		
+			<div class="ncstate-util-home">
+				<a href="#">&larr; ncsu.edu</a>
+			</div>
+
+			<div class="ncstate-util-options">
+
+				<div class="ncstate-util-toggle">
+					<a href="#" class="is-hidden">Show/Hide Quicklinks</a>
+				</div>
+			
+				<div class="ncstate-util-search">
+					<form>
+						<input type="search" placeholder="search ncsu.edu" />
+						<input type="submit" value="search" />
+					</form>
+				</div>
+				
+			</div><!-- end options -->
+		
+		</div><!-- end util wrapper -->
+	
+	</div><!-- end util -->
+	
+</body>
+
+</html>
