daa5cf3d45e89b2d7a2814302d4dcc86b13d1f3f
Andrew Matthews <apmatthe@ncsu.edu>
2014-04-18 15:58:50 -0400

    simplify demo and use CDN

--- demo.php
+++ demo.php
@@ -8,23 +8,7 @@
 
 <body>
 	
-	<h1>Lorem Ipsum Demo Page</h1>
-	
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In turpis tortor, vehicula a eros sodales, elementum consectetur elit. Nam quis auctor leo. Morbi augue nulla, elementum dictum elementum vel, congue vel turpis. Sed ac ullamcorper nunc. Phasellus aliquam, erat eget lacinia pellentesque, lorem velit ultricies arcu, sed placerat felis ante in purus. Cras ultricies placerat leo, quis dictum mi consequat vel. Pellentesque eu velit elit. In sagittis sem arcu, ac luctus diam aliquet ac. Maecenas et metus purus. Ut vel orci malesuada, imperdiet mi sed, sollicitudin quam. Etiam eu felis mauris. Maecenas mauris risus, malesuada rutrum lacus quis, mollis accumsan odio. Donec eget mollis libero. Donec accumsan mattis sapien non dapibus. Donec id sollicitudin nulla, vel auctor enim.</p>
-
-	<p>Phasellus tempus vestibulum adipiscing. Maecenas et nibh id velit feugiat ornare. Nam turpis risus, tempor ut lacinia a, feugiat in quam. Curabitur faucibus hendrerit dui, ultrices convallis mi facilisis eget. Suspendisse sit amet ipsum sed magna sodales facilisis. Suspendisse in tellus vel leo luctus elementum ac eget augue. In hac habitasse platea dictumst. Donec id massa sit amet arcu vestibulum tristique. Aliquam sed pretium tortor. Quisque quis urna ac nibh laoreet pellentesque ac a ipsum. Nulla pulvinar fringilla fringilla. Aenean sodales in mauris in lobortis. Duis fringilla quam vel ipsum aliquet egestas. Sed vehicula mattis porttitor.</p>
-	
-	<?php
-		
-	// params
-	
-	// color (defaults red)
-	// maxWidth (defaults 960)
-	// searchDomain (defaults '')
-		
-	?>
-	
-	<script src="http://webcommdev.com/utility/bb.php?searchDomain=bulletin.ncsu.edu&color=gray"></script>
+	<script src="https://cdn.ncsu.edu/brand-assets/utility-bar/bb.php"></script>
 	
 </body>
 </html>
