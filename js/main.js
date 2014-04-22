afde329b928f7060c1a0cfb7302018c6a0339e52
Andrew Matthews <apmatthe@ncsu.edu>
2014-04-22 15:56:07 -0400

    accessibility - display:none the quick links when closed to prevent tabbing.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
 		toggleView = d.getElementsByClassName('ncstate-brand-bar-links')[0],
 		searchForm = d.getElementsByClassName('ncstate-brand-bar-search-form')[0],
 		searchField = d.getElementsByClassName('ncstate-brand-bar-search-field')[0],
-		classPattern = /\bis-hidden\b/g;
+		classPattern = /\bis-hidden\b/g,
+		toggleState = 'hidden';
 
 	// Define Event Handlers
 	
@@ -15,9 +16,12 @@
 		e.preventDefault();
 		
 		if(classPattern.exec(toggleView.className)){
-			toggleView.className = toggleView.className.replace(classPattern,'');
+			toggleView.style.display = "block";
+			setTimeout(function(){toggleView.className = toggleView.className.replace(classPattern,'');}, 10);
+			toggleState = 'visible';
 		} else {
 			toggleView.className = toggleView.className + 'is-hidden';
+			toggleState = 'hidden';
 		}
 			
 	};
@@ -33,11 +37,20 @@
 		}
 
 	};
+
+	var handleTransitionEnd = function(e){
+		
+		if(toggleState === 'hidden'){
+			toggleView.style.display = "none";
+		}
+
+	};
 	
 	// Bind Event Handlers
 
 	toggleElm.addEventListener('click', handleToggleElement, false);
 	searchForm.addEventListener('submit', handleSearchSubmit, false);
+	toggleView.addEventListener('transitionend', handleTransitionEnd, false);
 	
 	
 })(window,document);
