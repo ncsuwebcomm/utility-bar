f3cfef9a2ed76f0ba27a41f0c5ff779fcd227f2c
Andrew Matthews <apmatthe@ncsu.edu>
2014-05-05 13:28:40 -0400

    run demo file off webcomm dev instead of cdn

--- demo.php
+++ demo.php
@@ -4,13 +4,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>Utility Prototype</title>
+	<script src="http://www.webcommdev.com/utility/load-utility-bar.js"></script>
 </head>
 
 <body>
 
-	<div id="ncstate-brand-bar"></div>
+	<div id="ncstate-utility-bar"></div>
 	<div id="second"></div>
-	<script src="https://cdn.ncsu.edu/brand-assets/utility-bar/ub.php"></script>
 	
 </body>
 </html>
