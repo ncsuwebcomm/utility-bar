6e7455bc26c44d1b297d4642d124c91be0b742b7
Andrew Matthews <apmatthe@ncsu.edu>
2014-08-29 14:45:26 -0400

    improve screen reader pronunciation

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 						<a id="ncstate-utility-bar-toggle-link" aria-label="Toggle resources" href="#">Resources</a>
 					</div>
 				
-					<div class="ncstate-utility-bar-search" role="search" aria-label="Search www.ncsu.edu">
+					<div class="ncstate-utility-bar-search" role="search" aria-label="Search w w w . n c s u . e d u">
 						
 						<form action="http://www.ncsu.edu/search" name="navsearchform" class="ncstate-utility-bar-search-form">
 	        
@@ -32,7 +32,7 @@
 						    <input id="navcof" name="cof" type="hidden" value="FORID:11">
 						    
 							<input class="ncstate-utility-bar-search-field" id="search-input" name="q" type="search"
-								placeholder="search ncsu.edu" aria-label="search ncsu.edu">
+								placeholder="search ncsu.edu" aria-label="search n c s u . e d u">
 						 	<input type="submit" id="search-submit" name="search-submit" value="search"
 								class="ncstate-utility-bar-search-submit">
 				
