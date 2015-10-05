e6d49759db5f1d94d1151f84dd6663082a2ca737
Jonathan Champ <jonathan_champ@ncsu.edu>
2015-10-05 16:29:47 -0400

    JRCHAMP Update demo test to test HTML injection

--- demo.php
+++ demo.php
@@ -4,7 +4,16 @@
 <head>
 	<meta charset="utf-8">
 	<title>Utility Prototype</title>
-	<script src="ub.php?googleCustomSearchCode=__SENTINEL__%27;alert%28%27Hi%20Guys!%27%29;//%27&maxWidth=1100%27&showBrick=1%27;alert%28%27Wider%20is%20better.%27%29;//%27&color=gray%27;alert%28%27Want%20a%20cookie?%20%27%20%2B%20document.cookie%29;//%27&asdf=blue"></script>
+<?php
+	$params = array(
+		'googleCustomSearchCode' => "\"></form></div></div></div></div><a onmouseover=\"alert('googleCustomSearchCode');\" style=\"position: absolute; width: 100%; height: 100%; background-color: blue; top: 0; left: 0; z-index: 99999;\">&nbsp;</a><div><div><div><div><form><input type=\"hidden\" name=\"fake\" value=\"",
+		'maxWidth' => "1100';alert('__maxWidth__');//\"></form></div></div></div></div><a onmouseover=\"alert('maxWidth');\" style=\"position: absolute; width: 100%; height: 100%; background-color: gray; top: 50px; left: 0; z-index: 99999;\">&nbsp;</a><div><div><div><div><form><input type=\"hidden\" name=\"fake\" value=\"",
+		'color' => "gray';alert('__color__ ' + document.cookie);//'\"></form></div></div></div></div><a onmouseover=\"alert('color');\" style=\"position: absolute; width: 100%; height: 100%; background-color: yellow; top: 150px; left: 0; z-index: 99999;\">&nbsp;</a><div><div><div><div><form><input type=\"hidden\" name=\"fake\" value=\"",
+		'placeholder' => "\"></form></div></div></div></div><a onmouseover=\"alert('placeholder');\" style=\"position: absolute; width: 100%; height: 100%; background-color: green; top: 100px; left: 0; z-index: 99999;\">&nbsp;</a><div><div><div><div><form><input type=\"hidden\" name=\"fake\" value=\"",
+		'showBrick' => "1';alert('__showBrick__');//\"></form></div></div></div></div><a onmouseover=\"alert('showBrick');\" style=\"position: absolute; width: 100%; height: 100%; background-color: red; top: 100px; left: 0; z-index: 99999;\">&nbsp;</a><div><div><div><div><form><input type=\"hidden\" name=\"fake\" value=\"",
+	);
+?>
+	<script src="ub.php?<?php echo htmlspecialchars(http_build_query($params)); ?>"></script>
 </head>
 
 <body>
