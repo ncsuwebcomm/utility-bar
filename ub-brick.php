3bbc6b267cc2873333c2f2ccb4e0b2c10d674364
MarissaGomes <mtgomes@ncsu.edu>
2015-12-18 09:03:44 -0500

    NCState Brick Option
    Adding option for NCState Brick - (*-brick files have cdn requests
    converted to localhost, for testing )

--- ub-brick.php
+++ ub-brick.php
@@ -24,6 +24,6 @@ var _ub = _ub || {};
 
 (function(){
 	var bar = document.createElement('script'); bar.type = 'text/javascript'; bar.async = true;
-	bar.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.ncsu.edu/brand-assets/utility-bar/load-utility-bar.js';
+	bar.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'localhost:8888/utility-bar/load-utility-bar.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(bar, s.nextSibling);
 })();
