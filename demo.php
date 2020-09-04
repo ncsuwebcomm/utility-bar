521f4a5e5bc29111d97a23c7b671d34efc81e995
Andrew Matthews <apmatthe@ncsu.edu>
2020-09-04 12:33:23 -0400

    add meta tag to demo page to support responsive testing

--- demo.php
+++ demo.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Utility Prototype</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<script src="dist/ub-php.js?googleCustomSearchCode=__SENTINEL__%27;alert%28%27Hi%20Guys!%27%29;//%27&maxWidth=1100%27&showBrick=1%27;alert%28%27Wider%20is%20better.%27%29;//%27&color=gray%27;alert%28%27Want%20a%20cookie?%20%27%20%2B%20document.cookie%29;//%27&asdf=blue"></script>
 </head>
 
